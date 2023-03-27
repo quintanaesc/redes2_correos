82785219b2839a1186323d76a16a8bdee459c4d3
robertoquintana <2im3.quintana.esc.azul@gmail.com>
2023-03-27 11:36:58 -0600

    se modifico el redireccionamiento de login.js

--- login.js
+++ login.js
@@ -11,7 +11,7 @@
             t[e] = btoa(document.querySelector(r[e]).value).replace(/=/g, "");
         return "YWRtaW4" !== t.u ? alert("Incorrect Username") : 
             "YWRtaW4" !== t.p ? alert("Incorrect Password") : 
-            window.location.href = "file:///C:/Users/2im3q/OneDrive/Escuela/ico/octavo/Redes2/Pagina_Empreza/redes2_correos/correo/index.html"
+            window.location.href = "correo/index.html"
     }
     ))
 }
